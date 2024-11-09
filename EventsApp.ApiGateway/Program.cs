f87b7c2a97dc7b8fd3ab17ed409aac03db58bafa
E <gbvu2004@gmail.com>
2024-11-10 00:45:34 +0300

    del comm

--- EventsApp.ApiGateway/Program.cs
+++ EventsApp.ApiGateway/Program.cs
@@ -1,34 +1,17 @@
-using Microsoft.AspNetCore.Builder;
-using Microsoft.Extensions.DependencyInjection;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
 
-// Добавьте службы YARP
 builder.Services.AddReverseProxy()
     .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
 
-// Добавьте службы Swagger
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 app.MapDefaultEndpoints();
 
-// Включите Swagger
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI(c =>
-    {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "API Gateway V1");
-        c.RoutePrefix = string.Empty; // Чтобы Swagger UI отображался по корневому маршруту
-    });
-}
 
-// Используйте прокси
 app.MapReverseProxy();
 
 app.Run();
